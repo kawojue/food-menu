529210cf096ea6a9de18fbaf6453a92d79ee6185
Raheem Kawojue <kawojue08@gmail.com>
2022-11-05 09:52:25 +0100

    Project setup

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.jsx'],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
