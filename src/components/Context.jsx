d518336cb521b6f9da4da16f7c56dcd8167f15bd
Raheem Kawojue <kawojue08@gmail.com>
2022-11-06 02:49:45 +0100

    Setup filtration

--- src/components/Context.jsx
+++ src/components/Context.jsx
@@ -1,11 +1,28 @@
 import { createContext, useState } from 'react'
+import data from '../data'
 
 const Context = createContext({})
 
 export const DataContext = ({ children }) => {
 
+    const [menu, setMenu] = useState(data)
+
+    const filtered = (category) => {
+        const newMenu = data.filter(food => {
+            if (category === 'all') {
+                return food
+            } else {
+                return food.category === category
+            }
+        })
+
+        setMenu(newMenu)
+    }
+
     return (
-        <Context.Provider value={{}}>
+        <Context.Provider value={{
+            menu, filtered
+        }}>
             {children}
         </Context.Provider>
     )
