c6d72780326f00516de98cbdb2fe80f1282803da
Raheem Kawojue <kawojue08@gmail.com>
2022-11-06 16:59:03 +0100

    Edit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 const Header = () => {
     return (
         <header>
-            <h1>Our Menu</h1>
+            <h1 className="md:text-5xl">Our Menu</h1>
             <div></div>
         </header>
     )
