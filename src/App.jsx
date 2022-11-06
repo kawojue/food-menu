07096c83102a86f778ebe77a4c26731319742d79
Raheem Kawojue <kawojue08@gmail.com>
2022-11-06 03:15:54 +0100

    Edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
-function App() {
+import Menu from './components/Menu'
+import Header from './components/Header'
+import Button from './components/Button'
 
+function App() {
   return (
     <main>
-      <p>Greetings - Hello, World!</p>
+      <Header />
+      <Button />
+      <Menu />
     </main>
   )
 }
